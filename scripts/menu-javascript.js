db62619dceafb3ac0e85de155320e150d1b65ab4
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2019-11-29 23:44:52 -0500

    menu style updated

--- scripts/menu-javascript.js
+++ scripts/menu-javascript.js
@@ -6,26 +6,46 @@ function pageReady () {
     console.log(mainPicture);
     var menuPictures = document.getElementsByClassName("menu-pictures");
     var appetizerPicture = document.getElementById("appetizer");
-    //var entreePicture = document.getElementById("entree");
     var mainCoursePicture = document.getElementById("main-course");
     var dessertPicture = document.getElementById("dessert");
     
-    //add Event Listener 
+    var appetizerMenu = document.getElementById("appetizer-list");
+    var mainDishMenu = document.getElementById("main-dish-list");
+    var dessertMenu = document.getElementById("dessert-list");
+    
+    
+    //add Event Listener
+    //onmousover
     appetizerPicture.onmouseover = changePic1;
-    //entreePicture.onmouseover = changePic2;
     mainCoursePicture.onmouseover = changePic3;
     dessertPicture.onmouseover = changePic4;
-    
+    //onclick
+    appetizerPicture.onclick = displayMenu1;
+    mainCoursePicture.onclick = displayMenu2;
+    dessertPicture.onclick = displayMenu3;
+    //function
+    //onmousover
     function changePic1 () {
         mainPicture.src = "image/big-appetizer.jpg";
     }
-    /*function changePic2 () {
-       mainPicture.src = "image/big-entree.jpg"; 
-    }*/
     function changePic3 () {
         mainPicture.src = "image/big-main-course.jpg";
     }
     function changePic4 () {
         mainPicture.src = "image/big-dessert.jpg";
     }
+    
+    //onclick
+    function displayMenu1 () {
+        appetizerMenu.style.display="block";        
+    }
+    function displayMenu2 () {
+        mainDishMenu.style.display="block";        
+    }
+    function displayMenu3 () {
+        dessertMenu.style.display="block";        
+    }
+    
+    
+    
 }
