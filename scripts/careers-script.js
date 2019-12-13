5f982c67e2e109c76a0cf4094dc02bf8cd0f7cb5
Yusha16 <chris.yushi@gmail.com>
2019-12-13 08:08:49 -0500

    Add more jobs.
    Create a array of jobs.

--- scripts/careers-script.js
+++ scripts/careers-script.js
@@ -3,7 +3,99 @@ $(document).ready(function() {
 	careersLoad();
 });
 
+var Job = {
+	name: "",
+	jobLocation: "",
+	salary: "",
+	id: -1,
+	description: "",
+	requirement: ""
+}
+
+function NewJob(name, jobLocation, salary, id, description, requirement) {
+	var newJob = Object.assign({}, Job);
+	newJob.name = name;
+	newJob.jobLocation = jobLocation;
+	newJob.salary = salary;
+	newJob.id = id;
+	newJob.description = description;
+	newJob.requirement = requirement;
+	return newJob;
+}
+
 function careersLoad() {
+	
+	//This will load all the jobs
+	var jobList = [];
+	LoadJobs();
+	function LoadJobs() {
+		jobList.push(NewJob("Waiter", "Vesuvius", "$20.00/hour", 1, "Take customer requests, help out where needed.", "High School Diploma"));
+		jobList.push(NewJob("Developer", "Stromboli", "$30.00/hour", 2, "We are looking for someone who is equal parts planner, problem solver, and analytical thinker.", "College Diploma"));
+		jobList.push(NewJob("Contact", "Kilauea", "$25.00/hour", 3, "Take phone calls, and resolve any issue from customer request", "College Diploma"));
+		jobList.push(NewJob("Chef", "Vesuvius", "$30.00/hour", 4, "Work with Corporate Head Chef on menu development and food procurement", "College Diploma"));
+		jobList.push(NewJob("Developer", "Vesuvius", "$35.00/hour", 5, "We are looking for someone who is equal parts planner, problem solver, and analytical thinker", "University Degree"));		
+		ShowJobs();
+	}
+	
+	function ShowJobs() {
+		var resultJobListResult = "";
+		for (var i = 0; i < jobList.length; i++) {
+			resultJobListResult += "<li class=\"result-job-list-item flex-container\">";
+			//Get the name
+			resultJobListResult += "<p>Job Name: " + jobList[i].name + "</p>";
+			//Get the location
+			resultJobListResult += "<p>Location: " + jobList[i].jobLocation + "</p>";
+			//Get the salary
+			resultJobListResult += "<p>Salary: " + jobList[i].salary + "</p>";
+			//Get the Job ID
+			resultJobListResult += "<p class=\"result-job-id\">Job ID: " + jobList[i].id + "</p>";
+			//Get the description
+			resultJobListResult += "<p class=\"result-job-detail\">Description: " + jobList[i].description + "</p>";
+			//Get the Reguirement
+			resultJobListResult += "<p class=\"result-job-detail\">Requirement: " + jobList[i].requirement + "</p>";				
+			resultJobListResult += "</li>";
+		}
+		$("#result-job-list").html(resultJobListResult);
+	}
+	
+	//The code below was attempt to use AJAX for opening a XML file
+	/*
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				var xmlDoc = xhr.responseXML;
+				var resultJobListResult = "";
+				
+				var xmlItems = xmlDoc.getElementsByTagName("job");
+				for (var i = 0; i < xmlItems.length; i++) {
+					resultJobListResult += "<li class=\"result-job-list-item flex-container\">";
+					//Get the name
+					resultJobListResult += "<p>Job Name: " + xmlItems[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + "</p>";
+					//Get the location
+					resultJobListResult += "<p>Location: " + xmlItems[i].getElementsByTagName("location")[0].childNodes[0].nodeValue + "</p>";
+					//Get the salary
+					resultJobListResult += "<p>Salary: " + xmlItems[i].getElementsByTagName("salary")[0].childNodes[0].nodeValue + "</p>";
+					//Get the Job ID
+					resultJobListResult += "<p class=\"result-job-id\">Job ID: " + xmlItems[i].getElementsByTagName("id")[0].childNodes[0].nodeValue + "</p>";
+					//Get the description
+					resultJobListResult += "<p class=\"result-job-detail\">Description: " + xmlItems[i].getElementsByTagName("description")[0].childNodes[0].nodeValue + "</p>";
+					//Get the Reguirement
+					resultJobListResult += "<p class=\"result-job-detail\">Reguirement: " + xmlItems[i].getElementsByTagName("requirement")[0].childNodes[0].nodeValue + "</p>";				
+					resultJobListResult += "</li>";
+				}
+				$("#result-job-list").html(resultJobListResult);
+			}
+			else {
+				console.log("Connection was unsuccessful");
+			}
+		}	
+	}
+	xhr.open("GET", "res/jobs.xml", true);
+	xhr.send(null);
+	*/
+	
+	
 	$(".job-content").hide();
 	
 	var toggleSpeed = 1000;
